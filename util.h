47b161b212a0990f6c9c0b3611c2ee5d5c696506
jgyimah <jude.gyimah87@gmail.com>
2019-07-01 14:12:22 +0100

    stream func def fix

--- util.h
+++ util.h
@@ -9,7 +9,7 @@ public:
 
 static std::string convertToTime ( long int input_seconds );
 static std::string getProgressBar(std::string percent);
-static std::ifstream getStream(std::string path);
+static std::ifstream getStream(std::string path, std::ifstream stream);
 };
 
 std::string Util::convertToTime (long int input_seconds){
