aca07ac5ee825432f797ef80b4ffd9af896a202e
jgyimah <jude.gyimah87@gmail.com>
2019-07-01 18:25:45 +0100

    get proc   func fix

--- Process.h
+++ Process.h
@@ -20,7 +20,7 @@ public:
         this->user = ProcessParser::getProcUser(pid);
         this->mem = ProcessParser::getVmSize(pid);
         this->cmd = ProcessParser::getCmd(pid);
-        this->up_time = ProcessParser::getProcUpTime(pid);
+        this->upTime = ProcessParser::getProcUpTime(pid);
         this->cpu  = ProcessParser::getCpuPercent(pid);
     }
     void setPid(int pid);
@@ -43,15 +43,5 @@ string Process::getProcess(){
     this->upTime = ProcessParser::getProcUpTime(this->pid);
     this->cpu = ProcessParser::getCpuPercent(this->pid);
 
-    return (this->pid + "   "
-            + this->user
-            + "   "
-            + this->mem.substr(0,5)
-            + "     "
-            + this->cpu.substr(0,5)
-            + "     "
-            + this->upTime.substr(0,5)
-            + "    "
-            + this->cmd.substr(0,30)
-            + "...");
+    return (this->pid + "   "+ this->user+ "   "+ this->mem.substr(0,5)+ "     "+ this->cpu.substr(0,5)+ "     "+ this->upTime.substr(0,5)+ "    "+ this->cmd.substr(0,30)+ "...");
 }
